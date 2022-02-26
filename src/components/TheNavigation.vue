a66894b00e60833cb7723d65f9b6166097a921c1
adamichelle <adamichelllle@gmail.com>
2022-02-26 00:01:16 -0500

    fix navigation styling issues

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -6,11 +6,19 @@
        aria-current="page">
         <img src="../assets/logo.png" alt="tf.js Playground" class="tp-logo">
       </router-link>
-      <button class="navbar-toggler" type="button"
-        data-bs-toggle="collapse" data-bs-target="#toggleableNavItems"
-        aria-controls="toggleableNavItems" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <div class="order-lg-3">
+        <router-link
+        class="btn tp-btn-amber me-3"
+        :to="{ name: 'Models' }"
+        v-if="showTryItOutButton">
+          Try it out
+        </router-link>
+        <button class="navbar-toggler" type="button"
+          data-bs-toggle="collapse" data-bs-target="#toggleableNavItems"
+          aria-controls="toggleableNavItems" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
       <div class="collapse navbar-collapse" id="toggleableNavItems">
         <ul :class="[{ 'me-auto': showTryItOutButton }, 'navbar-nav ms-auto mb-2 mb-lg-0']">
           <li class="nav-item">
@@ -24,10 +32,6 @@
           </li>
         </ul>
       </div>
-      <router-link
-       class="btn tp-btn-amber"
-       :to="{ name: 'Models' }"
-       v-if="showTryItOutButton">Try it out</router-link>
     </div>
   </nav>
 </template>
