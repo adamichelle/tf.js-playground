f27f79f892bbd81903f23268863fdc3fd9d39f3d
adamichelle <adamichelllle@gmail.com>
2022-03-01 03:01:19 -0500

    Changes to speech commands JS and add demo video

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -2,6 +2,17 @@
   <div>
     <div class="container vh-100 pt-3">
       <h1>A Sample Use Case Demo</h1>
+      <p>
+        In this demo, we will be exploring the Speech Command Recognizer
+        model.
+      </p>
+
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe loading="lazy"
+          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAE5tOdiXV8&#x2F;watch?embed"
+          allowfullscreen="allowfullscreen" allow="fullscreen">
+          </iframe>
+      </div>
     </div>
 
     <the-footer></the-footer>
